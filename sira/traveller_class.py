0b01cc0ea177cb5fd554f8fe3a2b3591c3252ae7
declan.bays <declan.bays@phe.gov.uk>
2020-06-30 12:33:34 +0100

    Added functionality to incorportate double testing

--- sira/traveller_class.py
+++ sira/traveller_class.py
@@ -8,7 +8,7 @@ class Traveller(object):
     is used to model the random time prior to their flight, that the individual
     became infected by this disease. 
     '''    
-    def __init__(self, exp_dist = None, **kwargs):
+    def __init__(self, exp_dist = None, asymp_prob = None, **kwargs):
         '''
         Initiate an instance of an infected passenger.
         
@@ -18,6 +18,10 @@ class Traveller(object):
             Distribution function to model the shape of the distribution of 
             times (in hours) taken from passengers becoming infected to 
             boarding flight to the destination country
+        asymp_prob : None or float
+            Float to describe the probability that simulated individual is 
+            going to be asymptomatic. If value is set to None, model does not
+            consider the possibility of asymptomatic infections
         **kwargs : dict
             Dictionary containing variables to which define the specific shape
             of the chosen input distirbution of time taken from becoming 
@@ -33,6 +37,12 @@ class Traveller(object):
             self.kwargs = {'loc' : 4*24, 'scale' : 10}
         else:
             self.kwargs = kwargs 
+        # Boolean variable to capture whether person has been detected by 
+        # methods of diseas detection
+        self.undetected = True
+        # Randomly allocate if individual is to be symptomatic or not
+        self.get_asymptomatic(asymp_prob)
+
             
     def get_depart_time(self):
         '''
@@ -42,3 +52,24 @@ class Traveller(object):
         '''
         flight_time =  self.exp_dist(**self.kwargs)
         return flight_time
+
+    def get_asymptomatic(self, asymp_prob):
+        '''
+        Class method to randomly decide whether modelled individual is to be
+        symptomatic or asymptomatic
+
+        Parameters
+        ----------
+        aysmp_prob : float or None
+            Float to describe the probability that the modelled individual is 
+            to be asymptomatic. If given value is None, function sets 
+            asymptomatic to True as default
+        '''
+        if  asymp_prob is None:
+            self.symptomatic = True
+        else:
+            draw = rand.random()
+            if draw < asymp_prob:
+                self.symptomatic = True
+            else:
+                self.symptomatic = False
